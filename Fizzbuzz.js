c75026c805052add2337f6bc90e913ee0c453cfe
Nancy Rojas <nancyrs22@hotmail.com>
2022-02-18 14:01:15 -0500

    Ejemplo y Practica 1

---
+++ Fizzbuzz.js
@@ -0,0 +1,23 @@
+
+for(i = 1; i<101;i++)
+{
+    if(i % 3 === 0)
+    {
+        if(i % 5 === 0)
+        {
+            console.log("FizzBuzz");
+        }
+        else
+        {
+            console.log("Fizz");
+        }
+    }
+    else if(i % 5 === 0)
+    {
+        console.log("Buzz");
+    }
+    else
+    {
+        console.log(i);
+    }
+}
---
+++ js-types.js
@@ -0,0 +1,17 @@
+// var num = 1;
+// var num2 = 2;
+// var suma = num + num2; 
+// var numFlotante = 1.5;
+// var text = "Esta es una variable texto";
+// var bool = false;
+
+// console.log(suma);
+// console.log(text);
+
+for(i = 1; i<101;i++)
+{
+    if(i % 3 == 3)
+    {
+        console.log("Fizz");
+    }
+}
