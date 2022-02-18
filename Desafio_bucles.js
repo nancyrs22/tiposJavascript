2ab7601fca40754fcb65bf9304ff9cbd58e9266d
Nancy Rojas <nancyrs22@hotmail.com>
2022-02-18 16:27:06 -0500

    Practifa desafio bucles

---
+++ Desafio_bucles.js
@@ -0,0 +1,52 @@
+
+//Imprimir impares 1-20 
+// for(i= 0; i<=20; i++)
+// {
+//     if(i%2 !== 0)
+//     {
+//         console.log(i)
+//     }
+// }
+
+//Imprimir impares 1-20 
+// for(i= 100; i>=0; i--)
+// {
+//     if(i%3 === 0)
+//     {
+//         console.log(i)
+//     }
+// }
+
+//Imprime la secuencia 
+// for(i = 4; i>-4; i-=1.5)
+// {
+//     console.log(i);
+// }
+
+//Sigma 
+// var sum = 0;
+
+// for(i=1; i<=100; i++)
+// {
+//     console.log(i)
+//     if(i<100)
+//     {
+//         console.log(" + ")
+//     }
+//     sum += i;
+// }
+// console.log(sum)
+
+//Factorial 
+// var product = 1;
+
+// for(i=1; i<=12; i++)
+// {
+//     console.log(i)
+//     if(i<12)
+//     {
+//         console.log(" * ")
+//     }
+//     product *= i;
+// }
+// console.log(product)
